362cc1111a42331d5cf5565e7e59fca690cc7c9d
Manasvi10 <86060802+Manasvi10@users.noreply.github.com>
2022-02-01 17:42:28 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+      apiKey: "AIzaSyDN_fOyXUbL6HI8q1ScwCOj7HrE5WdNZK4",
+      authDomain: "kwitter-90f48.firebaseapp.com",
+      databaseURL: "https://kwitter-90f48-default-rtdb.firebaseio.com",
+      projectId: "kwitter-90f48",
+      storageBucket: "kwitter-90f48.appspot.com",
+      messagingSenderId: "124874196571",
+      appId: "1:124874196571:web:9d2838a0d0673b81b4617e"
+    };
+    
+   
+    firebase.initializeApp(firebaseConfig);
+
+    
